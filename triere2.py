851b1687632408130146486185aca8bcdc0b923a
Rodion Balaniuc <70860169+Rxdxxn@users.noreply.github.com>
2022-12-07 00:43:12 +0200

    Update triere2.py

--- triere2.py
+++ triere2.py
@@ -7,11 +7,9 @@ equal2=True
 for i in n1:
     if i not in n2:
         equal1=False
-        break
 for i in n2:
     if i not in n1:
         equal2=False
-        break
 if(equal2 and equal1):
     print("Numerele sunt prietenoase")
 else:
