74ef21fef1b849b77d981a906b73319a919ca646
Rodion Balaniuc <70860169+Rxdxxn@users.noreply.github.com>
2022-12-15 23:17:02 +0200

    Update triere.py

--- triere.py
+++ triere.py
@@ -1,24 +1,29 @@
 #Se consideră numerele naturale din mulţimea {m, …, n}.
 #Elaboraţi un program care determină pentru cîte numere q din această mulţime vor avea numarul x in structura.
+#Sa se afiseze numerele determinate.
 
+m = int(input('Introduceti limita inferioara: '))
+n = int(input('Introduceti limita superioara: '))
+x = int(input('Introduceti cifra cautata: '))
+lst = []
+rez = []
 
-n = int(input('Introduceti limita maxima pentru interval:'))
-m=int(input('Introduceti limita minima pentru interval:'))
-x=int(input('Introduceti cifra corespunzatoare:'))
-k=0
-nm=list(range(m,n))
+for i in range(m,n+1):
+    lst.append(i)
 
-def Num(a,b):
-    for i in b:
-        if i not in b:
-            return False
-        else:
+def verificareSolutie(k):
+    while k != 0:
+        if k%10 == x:
             return True
+        k //= 10
+        return False
 
-def da(a,b):
-    for i in a:
-        if i in b:
-            return k+1
+def adaugareSolutie(k):
+    global rez
+    rez.append(k)
 
+for i in lst:
+    if verificareSolutie(i):
+        adaugareSolutie(i)
 
-print('Conditia se satisface pentru',da(nm,x),'numere!')
+print(f'Elementele care contin {x} sunt {len(rez)} \n{rez}')
